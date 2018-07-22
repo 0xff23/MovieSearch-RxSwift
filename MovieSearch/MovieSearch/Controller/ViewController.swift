24ea76101ec73014d5be1f28e3dffbf7bab61981
Kirill <kvga923@gmail.com>
2018-07-22 15:25:18 -0400

    Initial commit

---
+++ MovieSearch/MovieSearch/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Kirill G on 7/22/18.
+//  Copyright © 2018 ns-ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
